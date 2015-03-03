7c1f512c247c80a29e08e007842529ccfd1a247d
pierangeloc <pierangeloc@gmail.com>
2015-03-03 11:52:07 +0100

    Initial commit

---
+++ src/main/java/com/pierangeloc/java8/lambdas/Collections.java
@@ -0,0 +1,15 @@
+package com.pierangeloc.java8.lambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pierangeloc on 15-1-15.
+ */
+public class Collections {
+
+    public void mapList() {
+        List<String> list = new ArrayList<>();
+
+    }
+}
